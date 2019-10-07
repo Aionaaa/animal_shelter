40d62ceaadcbd0fe73e07a6acf87ba6c1c1b0880
Iona Grant <ionagrant92@gmail.com>
2019-10-07 17:22:01 +0100

    started working on witch model

--- models/witch.rb
+++ models/witch.rb
@@ -4,7 +4,13 @@ class Witch
 
   attr_reader(:first_name, :last_name)
 
+  def initialize(options)
+    @id = options['id'].to_i if options['id']
+    @first_name = options['first_name']
+    @last_name = options['last_name']
+  end
 
+  
 
 
 
