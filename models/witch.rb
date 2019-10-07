5eb81617aa2fae026a7c2a2350364c4ee6303061
Iona Grant <ionagrant92@gmail.com>
2019-10-07 17:48:41 +0100

    completed save and all for witch model

--- models/witch.rb
+++ models/witch.rb
@@ -10,9 +10,29 @@ class Witch
     @last_name = options['last_name']
   end
 
+  def save()
+    sql = "INSERT INTO witches
+    (
+      first_name,
+      last_name
+    )
+    VALUES
+    (
+      $1, $2
+    )
+    RETURNING id"
+    values = [@first_name, @last_name]
+    results = SqlRunner.run(sql, values)
+    @id = results.first()['id'].to_i
+  end
+
+  def self.all()
+    sql = "SELECT * FROM witches"
+    results = SqlRunner.run(sql)
+    return results.map{|hash| Witch.new(hash)}
+  end
+
   
-
-
 
 
   def format_name
