0e0e613f8fc7000612cbd39c32598cbfff5c6d9b
Iona Grant <ionagrant92@gmail.com>
2019-10-08 11:33:49 +0100

    working on app rb

--- app.rb
+++ app.rb
@@ -6,3 +6,41 @@ require_relative('controllers/familiars_controller')
 get '/' do
   erb( :index )
 end
+
+get '/familiars' do
+  @familiars = Familiar.all
+  erb(:index)
+end
+
+get '/familiars/new' do
+  @witches = Witch.all
+  erb(:new)
+end
+
+post '/familiars' do
+  Familiar.new(params).save
+  redirect to '/familiars'
+end
+
+get '/familiars/:id' do
+  @familiar = Familiar.find(params['id'])
+  erb(:show)
+end
+
+get '/familiars/:id/edit' do
+  @witches = Witch.all
+  @familiar = Familiar.find(params['id'])
+  erb(:edit)
+end
+
+post '/familiars/:id' do
+  familiar = Familiar.new(params)
+  Familiar.update
+  redirect to "/familiars/#{params['id']}"
+end
+
+post '/familiars/:id/delete' do
+  familiar = Familiar.find(params['id'])
+  familiar.delete
+  redirect to '/familiars'
+end
